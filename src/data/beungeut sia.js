428c5457d347b7a21605258535a2ea7b7bdf76c5
husen <husenmalik7@gmail.com>
2026-05-14 08:55:58 +0700

    data g

---
+++ src/data/beungeut sia.js
@@ -0,0 +1,162 @@
+ada contoh data kayak gini
+
+{
+    cells: generateCells(952, 772, 960, 777),
+    id: 8448,
+    circle_code: 'F-60ab',
+    name: 'Gajahkecil',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/Gajahkecil.jpg',
+    circle_facebook: null,
+    circle_instagram: 'https://www.instagram.com/gajahkecil.crafts',
+    circle_twitter: null,
+    circle_other_socials: null,
+    sampleworks_images: null,
+    day: 'Both Days',
+    circle_type: '2 Space(s)',
+  },
+
+kamu bisa ga bantuin aku masuk masukin data ini (untuk bagian generatecellnya)
+
+
+
+903,694,912,698
+908,694,912,698
+903,694,907,698
+903,706,907,716
+903,718,907,740
+903,742,907,753
+903,772,912,777
+
+
+
+
+ke sini
+
+
+ {
+    cells: generateCells(865, 694, 874, 698),
+    id: 8509,
+    circle_code: 'G-54ab (SAT)',
+    name: 'Anggrek Mekar Kasablanka',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/Anggrek Mekar Kasablanka.jpg',
+    circle_facebook: 'https://web.facebook.com/caberavvit/',
+    circle_instagram: 'instagram.com/kkuroari',
+    circle_twitter: 'https://x.com/caberavvit',
+    circle_other_socials: null,
+    sampleworks_images: [
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/141d29ec-9dd4-4aaf-88b8-a51f3f13c55c/1374509d-29ec-46e4-9620-6a0b832db84a/G-54ab (SAT).png',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/141d29ec-9dd4-4aaf-88b8-a51f3f13c55c/2b937680-b2f4-47ff-a030-ceabd817f84c/G-54ab (SAT).png',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/141d29ec-9dd4-4aaf-88b8-a51f3f13c55c/c07ce335-102d-48fc-a135-19be6d70888a/G-54ab (SAT).png',
+    ],
+    day: 'SAT',
+    circle_type: '2 Space(s)',
+  },
+  {
+    cells: generateCells(870, 694, 874, 698),
+    id: 8510,
+    circle_code: 'G-54a (SUN)',
+    name: 'Jump!! Boat!',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/Jump!! Boat!.jpg',
+    circle_facebook: null,
+    circle_instagram: 'https://www.instagram.com/jumpboat.time?igsh=dWJmNXYzMHVzb2Y0',
+    circle_twitter: null,
+    circle_other_socials: 'https://msha.ke/chocosenja#links',
+    sampleworks_images: [
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/41f2a0cd-176f-4c0d-ab39-042a3165fb5a/G-54a (SUN).png',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/a240e002-bb93-4f0d-8534-8e07d651a6dd/G-54a (SUN).png',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/a5daf2dc-5257-44cc-ba13-9c7bd2282605/G-54a (SUN).png',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/80f9b612-930a-42d1-b187-6dc701c34047/G-54a (SUN).png',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/c3395f67-5674-48e1-a08c-8121e9e184ff/G-54a (SUN).png',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/504aff46-0aec-46e1-8548-bfa84baec4b5/G-54a (SUN).jpg',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/a1597721-fa77-4716-a9d3-cccf38241d3b/G-54a (SUN).jpg',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/a932b7fd-8971-4001-9721-cc2b3e12fd38/G-54a (SUN).jpg',
+      'https://kumxjefxtrrpzalmwvvr.supabase.in/storage/v1/object/public/circle-sampleworks-22/bf126059-2454-4c10-83ad-9eb6f78c4f02/f54357b9-7714-4587-b63b-45986d41f829/G-54a (SUN).jpg',
+    ],
+    day: 'SUN',
+    circle_type: '1 Space(s)',
+  },
+  {
+    cells: generateCells(865, 694, 869, 698),
+    id: 8511,
+    circle_code: 'G-54b (SUN)',
+    name: 'Silver Cafe',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/Silver Cafe.jpg',
+    circle_facebook: null,
+    circle_instagram: 'https://www.instagram.com/dayandra_owo?igsh=MTAwMGR2cnozNTY4eg==',
+    circle_twitter: 'https://x.com/Soul_Reaper_V_',
+    circle_other_socials: null,
+    sampleworks_images: null,
+    day: 'SUN',
+    circle_type: '1 Space(s)',
+  },
+  {
+    cells: generateCells(865, 706, 869, 716),
+    id: 8512,
+    circle_code: 'G-55ab',
+    name: 'Freaky Mart',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/Freaky Mart.jpg',
+    circle_facebook: '-',
+    circle_instagram: 'https://www.instagram.com/ruesttt/',
+    circle_twitter: 'https://x.com/ruestttt',
+    circle_other_socials: 'https://linktr.ee/ruesttt',
+    sampleworks_images: null,
+    day: 'Both Days',
+    circle_type: '2 Space(s)',
+  },
+  {
+    cells: generateCells(865, 718, 869, 740),
+    id: 8513,
+    circle_code: 'G-56ab/G-57ab',
+    name: 'STAY WARAS',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/STAY WARAS.jpg',
+    circle_facebook: null,
+    circle_instagram:
+      'instagram.com/_lilpage instagram.com/taomatow instagram.com/yant2k instagram.com/aleal249 Instagram.com/rafwacchi Instagram.com/kiull90 Instagram.com/binero10 Instagram.com/an__zoe',
+    circle_twitter:
+      'x.com/_lilpage x.com/taomatow x.com/sshiroixx x.com/yant2k x.com/Aleal249 X.com/rafwacchi X.com/kiull90 X.com/Binero_10 X.com/an__zoe',
+    circle_other_socials: null,
+    sampleworks_images: null,
+    day: 'Both Days',
+    circle_type: '4 Space(s)',
+  },
+  {
+    cells: generateCells(865, 742, 869, 753),
+    id: 8514,
+    circle_code: 'G-58ab/G-59ab',
+    name: 'Astristicworks',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/Astristicworks.jpg',
+    circle_facebook: null,
+    circle_instagram: 'https://www.instagram.com/astristicwork.shop/?hl=id',
+    circle_twitter: 'https://x.com/astristicworks',
+    circle_other_socials: null,
+    sampleworks_images: null,
+    day: 'Both Days',
+    circle_type: '4 Space(s)',
+  },
+  {
+    cells: generateCells(865, 772, 874, 777),
+    id: 8515,
+    circle_code: 'G-60ab',
+    name: 'Makonyan',
+    circle_cut:
+      'https://kumxjefxtrrpzalmwvvr.supabase.co/storage/v1/object/public/circle-cut-22/Makonyan.jpg',
+    circle_facebook: null,
+    circle_instagram: 'https://www.instagram.com/erasartt/',
+    circle_twitter: null,
+    circle_other_socials: 'https://makonyanclub.carrd.co/',
+    sampleworks_images: null,
+    day: 'Both Days',
+    circle_type: '2 Space(s)',
+  },
+
+
+
+
+urutannya udah pas ya, tinggal masuk masukin aja
